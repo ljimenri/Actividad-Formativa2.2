8a28eee9691f7280b2258dde5b6f457b4555a205
JAPMelendez <jparadamuft@gmail.com>
2021-05-03 16:40:10 -0400

    Se integra los cambios realizados en el Personas y Validadores

--- src/main/java/com/everis/data/models/Persona.java
+++ src/main/java/com/everis/data/models/Persona.java
@@ -1,5 +1,92 @@
 package com.everis.data.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="persona")
 public class Persona {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Size(min=4, max=50)
+private String nombre;
+@Size(min=4, max=50)
+private String apellido;
+@Size(min=10, max=30)
+private String email;
+@Size(min=10, max=12)
+private String correo;
+
+@Column(updatable=false)
+@DateTimeFormat(pattern="yyyy-MM-dd")
+private Date createdAt;
+
+@DateTimeFormat(pattern="yyyy-MM-dd")
+private Date updatedAt;
+
+public Persona(
+@Size(min = 4, max = 50) String nombre,
+@Size(min = 4, max = 50) String apellido,
+@Size(min = 10, max = 30) String email,
+@Size(min = 10, max = 12) String correo) {
+super();
+this.nombre = nombre;
+this.apellido = apellido;
+this.email = email;
+this.correo = correo;
+}
+
+public String getNombre() {
+return nombre;
+}
+
+public void setNombre(String nombre) {
+this.nombre = nombre;
+}
+
+public String getApellido() {
+return apellido;
+}
+
+public void setApellido(String apellido) {
+this.apellido = apellido;
+}
+
+public String getEmail() {
+return email;
+}
+
+public void setEmail(String email) {
+this.email = email;
+}
+
+public String getCorreo() {
+return correo;
+}
+
+public void setCorreo(String correo) {
+this.correo = correo;
+}
+
+@PrePersist
+protected void onCreate(){
+this.createdAt = new Date();
+}
+@PreUpdate
+protected void onUpdate(){
+this.updatedAt = new Date();
+}
 
 }
